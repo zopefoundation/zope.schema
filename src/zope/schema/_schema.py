efc4040992cc147a44f998173dc7918f739e9f20
Christian Zagrodnick <cz@gocept.com>
2007-08-24 06:43:34 +0000

    whitespace

--- src/zope/schema/_schema.py
+++ src/zope/schema/_schema.py
@@ -22,6 +22,7 @@ def getFieldNames(schema):
     from zope.schema.interfaces import IField
     return [name for name in schema if IField.providedBy(schema[name])]
 
+
 def getFields(schema):
     """Return a dictionary containing all the Fields in a schema.
     """
@@ -33,6 +34,7 @@ def getFields(schema):
             fields[name] = attr
     return fields
 
+
 def getFieldsInOrder(schema,
                      _fieldsorter=lambda x, y: cmp(x[1].order, y[1].order)):
     """Return a list of (name, value) tuples in native schema order.
@@ -40,6 +42,7 @@ def getFieldsInOrder(schema,
     fields = getFields(schema).items()
     fields.sort(_fieldsorter)
     return fields
+
 
 def getFieldNamesInOrder(schema):
     """Return a list of all the Field names in a schema in schema order.
