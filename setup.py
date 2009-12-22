b6be222a5b85853a06ad3c402109db087c34ee6c
Hanno Schlichting <hanno@hannosch.eu>
2009-12-22 14:08:19 +0000

    Prepare zope.schema 3.6.0.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.schema',
-      version = '3.6.0dev',
+      version = '3.6.0',
       url='http://pypi.python.org/pypi/zope.schema',
       license='ZPL 2.1',
       description='zope.interface extension for defining data schemas',
