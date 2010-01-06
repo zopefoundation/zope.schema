aa61552d72e84f169b14b149f760670d10af7ba9
Chris McDonough <chrism@plope.com>
2010-01-06 03:08:17 +0000

    vb.

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ def alltests():
     return suite
 
 setup(name='zope.schema',
-      version = '3.6.1dev',
+      version = '3.6.2dev',
       url='http://pypi.python.org/pypi/zope.schema',
       license='ZPL 2.1',
       description='zope.interface extension for defining data schemas',
