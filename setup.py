6272ea5d7b2f2fc9481120c9b9c423dee31ac846
Tres Seaver <tseaver@palladion.com>
2010-04-06 02:57:37 +0000

    Comply with repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -67,7 +67,7 @@ setup(name='zope.schema',
       url='http://pypi.python.org/pypi/zope.schema',
       license='ZPL 2.1',
       description='zope.interface extension for defining data schemas',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       long_description=(read('src', 'zope', 'schema', 'README.txt')
                         + '\n\n' +
