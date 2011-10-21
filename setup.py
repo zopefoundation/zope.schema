7c91f13f566f468a7e7a02fc0185e2f5ca927e7b
Brian Sutherland <brian@vanguardistas.net>
2011-10-21 14:41:11 +0000

    Set version number of next release to 4.0.0
    Justified by Python 3.2 support, a new dependency 'six' and the removal
    of Python 2.5 support.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ def alltests():
     return suite
 
 setup(name='zope.schema',
-      version = '3.8.2dev',
+      version = '4.0.0dev',
       url='http://pypi.python.org/pypi/zope.schema',
       license='ZPL 2.1',
       description='zope.interface extension for defining data schemas',
