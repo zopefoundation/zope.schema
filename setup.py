e4fcacb127824c9ee0e4bd744bac35c8f7875cf1
Jan-Carel Brand <jc@opkode.com>
2012-03-23 12:31:29 +0000

    Back to development: 4.1.2

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ if sys.version_info < (2, 7):
     REQUIRES += ['ordereddict']
 
 setup(name='zope.schema',
-      version='4.1.1',
+      version='4.1.2.dev0',
       url='http://pypi.python.org/pypi/zope.schema',
       license='ZPL 2.1',
       description='zope.interface extension for defining data schemas',
