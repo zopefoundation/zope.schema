3966ebecb9c3553d7159b375933353e233e338cb
Tres Seaver <tseaver@palladion.com>
2012-04-20 19:20:39 +0000

    Rip out doctest-only 'print_' compatibility shim.

--- src/zope/schema/_compat.py
+++ src/zope/schema/_compat.py
@@ -12,7 +12,6 @@ if PY3: #pragma NO COVER
     text_type = str
     binary_type = bytes
     integer_types = int,
-    print_ = getattr(builtins, "print")
 else: #pragma NO COVER
     def b(s):
         return s
@@ -22,48 +21,3 @@ else: #pragma NO COVER
     text_type = unicode
     binary_type = str
     integer_types = (int, long)
-
-    def print_(*args, **kwargs):
-        """The new-style print function."""
-        fp = kwargs.pop("file", sys.stdout)
-        if fp is None:
-            return
-        def write(data):
-            if not isinstance(data, basestring):
-                data = str(data)
-            fp.write(data)
-        want_unicode = False
-        sep = kwargs.pop("sep", None)
-        if sep is not None:
-            if isinstance(sep, unicode):
-                want_unicode = True
-            elif not isinstance(sep, str):
-                raise TypeError("sep must be None or a string")
-        end = kwargs.pop("end", None)
-        if end is not None:
-            if isinstance(end, unicode):
-                want_unicode = True
-            elif not isinstance(end, str):
-                raise TypeError("end must be None or a string")
-        if kwargs:
-            raise TypeError("invalid keyword arguments to print()")
-        if not want_unicode:
-            for arg in args:
-                if isinstance(arg, unicode):
-                    want_unicode = True
-                    break
-        if want_unicode:
-            newline = unicode("\n")
-            space = unicode(" ")
-        else:
-            newline = "\n"
-            space = " "
-        if sep is None:
-            sep = space
-        if end is None:
-            end = newline
-        for i, arg in enumerate(args):
-            if i:
-                write(sep)
-            write(arg)
-        write(end)
