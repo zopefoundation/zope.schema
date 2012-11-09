8a338119ac0872451841d78a011308b21ca892dc
David Glick <dglick@gmail.com>
2012-11-09 20:08:30 +0000

    Prepare zope.schema 4.2.1.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ if sys.version_info < (2, 7):
 TESTS_REQUIRE = ['zope.testing']
 
 setup(name='zope.schema',
-      version='4.2.1.dev',
+      version='4.2.1',
       url='http://pypi.python.org/pypi/zope.schema',
       license='ZPL 2.1',
       description='zope.interface extension for defining data schemas',
