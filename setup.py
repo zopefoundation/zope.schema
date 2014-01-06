6329df6c9dc81bc4432c395866984355037de35a
Adam Groszer <agroszer@gmail.com>
2014-01-06 14:22:35 +0100

    Back to development: 4.3.4

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ TESTS_REQUIRE = ['zope.testing']
 
 setup(
     name='zope.schema',
-    version='4.3.3',
+    version='4.3.4.dev0',
     url='http://pypi.python.org/pypi/zope.schema',
     license='ZPL 2.1',
     description='zope.interface extension for defining data schemas',
