zopefoundation/zope.schema

commit afa7e25d371e18cffb14b8b42224ba1eb9345be6

Drop support for Python < 3.7 (#118)

src/zope/schema/_messageid.py
@@ -15,6 +15,6 @@
 try:
     from zope.i18nmessageid import MessageFactory
 except ImportError:  # pragma: no cover
-    from zope.schema._compat import text_type as _
+    MessageFactory = str
 else:  # pragma: no cover
     _ = MessageFactory("zope")

.github/workflows/tests.yml
@@ -21,15 +21,11 @@ jobs:
         config:
         # [Python version, tox env]
         - ["3.9",   "lint"]
-        - ["2.7",   "py27"]
-        - ["3.5",   "py35"]
-        - ["3.6",   "py36"]
         - ["3.7",   "py37"]
         - ["3.8",   "py38"]
         - ["3.9",   "py39"]
         - ["3.10",  "py310"]
         - ["3.11",  "py311"]
-        - ["pypy-2.7", "pypy"]
         - ["pypy-3.7", "pypy3"]
         - ["3.9",   "docs"]
         - ["3.9",   "coverage"]
@@ -60,7 +56,7 @@ jobs:
     - name: Coverage
       if: matrix.config[1] == 'coverage'
       run: |
-        pip install coveralls coverage-python-version
+        pip install coveralls
         coveralls --service=github
       env:
         GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}

.meta.toml
@@ -2,11 +2,11 @@
 # https://github.com/zopefoundation/meta/tree/master/config/pure-python
 [meta]
 template = "pure-python"
-commit-id = "200573eb414d2228d463da3de7d71a6d6335a704"
+commit-id = "019f22e7c524781b052f2680f90f2ac1195e73e2"
 
 [python]
 with-pypy = true
-with-legacy-python = true
+with-legacy-python = false
 with-docs = true
 with-sphinx-doctests = true
 with-windows = false

setup.cfg
@@ -1,7 +1,7 @@
 # Generated from:
 # https://github.com/zopefoundation/meta/tree/master/config/pure-python
 [bdist_wheel]
-universal = 1
+universal = 0
 
 [flake8]
 doctests = 1

setup.py
@@ -63,11 +63,7 @@ setup(
         "License :: OSI Approved :: Zope Public License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -78,7 +74,7 @@ setup(
         "Framework :: Zope :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.7',
     include_package_data=True,
     zip_safe=False,
     tests_require=TESTS_REQUIRE,

src/zope/schema/_bootstrapfields.py
@@ -53,9 +53,6 @@ from zope.schema._bootstrapinterfaces import TooShort
 from zope.schema._bootstrapinterfaces import TooSmall
 from zope.schema._bootstrapinterfaces import ValidationError
 from zope.schema._bootstrapinterfaces import WrongType
-from zope.schema._compat import PY2
-from zope.schema._compat import integer_types
-from zope.schema._compat import text_type
 
 
 class _NotGiven(object):
@@ -524,7 +521,7 @@ class MinMaxLen(object):
 @implementer(IFromUnicode)
 class Text(MinMaxLen, Field):
     """A field containing text used for human discourse."""
-    _type = text_type
+    _type = str
     unicode_normalization = 'NFC'
 
     def __init__(self,  *args, **kw):
@@ -552,7 +549,7 @@ class Text(MinMaxLen, Field):
         zope.schema._bootstrapinterfaces.ConstraintNotSatisfied:
             (u'foo spam', '')
         """
-        if isinstance(value, text_type):
+        if isinstance(value, str):
             if self.unicode_normalization:
                 value = unicodedata.normalize(
                     self.unicode_normalization, value)
@@ -759,13 +756,8 @@ class Number(Orderable, Field):
         finally:
             last_exc = None
 
-    # On Python 2, native strings are byte strings, which is
-    # what the converters expect, so we don't need to do any decoding.
-    if PY2:  # pragma: PY2
-        fromBytes = fromUnicode
-    else:    # pragma: PY3
-        def fromBytes(self, value):
-            return self.fromUnicode(value.decode('utf-8'))
+    def fromBytes(self, value):
+        return self.fromUnicode(value.decode('utf-8'))
 
 
 class Complex(Number):
@@ -930,7 +922,7 @@ class Int(Integral):
     """A field representing a native integer type. and implementing
     :class:`zope.schema.interfaces.IInt`.
     """
-    _type = integer_types
+    _type = int
     _unicode_converters = (int,)
 
 

src/zope/schema/_compat.py
@@ -1,39 +1,0 @@
-import sys
-
-
-PY3 = sys.version_info[0] >= 3
-PY2 = not PY3
-
-if PY3:  # pragma: no cover
-
-    string_types = str,
-    text_type = str
-    binary_type = bytes
-    integer_types = int,
-
-    def non_native_string(x):
-        if isinstance(x, bytes):
-            return x
-        return bytes(x, 'unicode_escape')
-
-    def make_binary(x):
-        if isinstance(x, bytes):
-            return x
-        return x.encode('ascii')
-
-else:  # pragma: no cover
-
-    string_types = (basestring, )   # noqa: F821
-    text_type = unicode             # noqa: F821
-    binary_type = str               # noqa: F821
-    integer_types = (int, long)     # noqa: F821
-
-    def non_native_string(x):
-        if isinstance(x, unicode):  # noqa: F821
-            return x
-        return unicode(x, 'unicode_escape')  # noqa: F821
-
-    def make_binary(x):
-        if isinstance(x, str):
-            return x
-        return x.encode('ascii')

src/zope/schema/_field.py
@@ -15,13 +15,8 @@
 """
 __docformat__ = 'restructuredtext'
 
-try:
-    from collections import abc
-except ImportError:  # pragma: PY2
-    # Python 2
-    import collections as abc
-
 import re
+from collections import abc
 from datetime import date
 from datetime import datetime
 from datetime import time
@@ -53,11 +48,6 @@ from zope.schema._bootstrapfields import Real
 from zope.schema._bootstrapfields import Text
 from zope.schema._bootstrapfields import TextLine
 from zope.schema._bootstrapfields import _NotGiven
-from zope.schema._compat import PY3
-from zope.schema._compat import binary_type
-from zope.schema._compat import make_binary
-from zope.schema._compat import string_types
-from zope.schema._compat import text_type
 from zope.schema.fieldproperty import FieldProperty
 from zope.schema.interfaces import IASCII
 from zope.schema.interfaces import IURI
@@ -174,19 +164,18 @@ class implementer_if_needed(object):
 @implementer(ISourceText)
 class SourceText(Text):
     __doc__ = ISourceText.__doc__
-    _type = text_type
+    _type = str
 
 
 @implementer(IBytes, IFromUnicode, IFromBytes)
 class Bytes(MinMaxLen, Field):
     __doc__ = IBytes.__doc__
-
-    _type = binary_type
+    _type = bytes
 
     def fromUnicode(self, value):
         """ See IFromUnicode.
         """
-        return self.fromBytes(make_binary(value))
+        return self.fromBytes(value.encode('ascii'))
 
     def fromBytes(self, value):
         self.validate(value)
@@ -194,7 +183,7 @@ class Bytes(MinMaxLen, Field):
 
 
 @implementer_if_needed(INativeString, IFromUnicode, IFromBytes)
-class NativeString(Text if PY3 else Bytes):
+class NativeString(Text):
     """
     A native string is always the type `str`.
 
@@ -208,11 +197,10 @@ class NativeString(Text if PY3 else Bytes):
     """
     _type = str
 
-    if PY3:  # pragma: no branch
-        def fromBytes(self, value):
-            value = value.decode('utf-8')
-            self.validate(value)
-            return value
+    def fromBytes(self, value):
+        value = value.decode('utf-8')
+        self.validate(value)
+        return value
 
 
 @implementer(IASCII)
@@ -237,7 +225,7 @@ class BytesLine(Bytes):
 
 
 @implementer_if_needed(INativeStringLine, IFromUnicode, IFromBytes)
-class NativeStringLine(TextLine if PY3 else BytesLine):
+class NativeStringLine(TextLine):
     """
     A native string is always the type `str`; this field excludes
     newlines.
@@ -252,11 +240,10 @@ class NativeStringLine(TextLine if PY3 else BytesLine):
     """
     _type = str
 
-    if PY3:  # pragma: no branch
-        def fromBytes(self, value):
-            value = value.decode('utf-8')
-            self.validate(value)
-            return value
+    def fromBytes(self, value):
+        value = value.decode('utf-8')
+        self.validate(value)
+        return value
 
 
 @implementer(IASCIILine)
@@ -397,7 +384,7 @@ class Choice(Field):
     def __init__(self, values=None, vocabulary=None, source=None, **kw):
         """Initialize object."""
         if vocabulary is not None:
-            if (not isinstance(vocabulary, string_types)
+            if (not isinstance(vocabulary, str)
                     and not IBaseVocabulary.providedBy(vocabulary)):
                 raise ValueError('vocabulary must be a string or implement '
                                  'IBaseVocabulary')
@@ -420,7 +407,7 @@ class Choice(Field):
         self.vocabularyName = None
         if values is not None:
             self.vocabulary = SimpleVocabulary.fromValues(values)
-        elif isinstance(vocabulary, string_types):
+        elif isinstance(vocabulary, str):
             self.vocabularyName = vocabulary
         else:
             if (not ISource.providedBy(vocabulary)
@@ -500,17 +487,15 @@ class _StrippedNativeStringLine(NativeStringLine):
 
     def fromUnicode(self, value):
         v = value.strip()
-        # On Python 2, self._type is bytes, so we need to encode
-        # unicode down to ASCII bytes. On Python 3, self._type is
-        # unicode, but we don't want to allow non-ASCII values, to match
-        # Python 2 (our regexs would reject that anyway.)
+        # self._type is unicode, but we don't want to allow non-ASCII
+        # values, to match the old behaviour on Python 2 (our regexes would
+        # reject that anyway).
         try:
             v = v.encode('ascii')  # bytes
         except UnicodeEncodeError:
             raise self._invalid_exc_type(value).with_field_and_value(
                 self, value)
-        if not isinstance(v, self._type):  # pragma: no branch
-            v = v.decode('ascii')
+        v = v.decode('ascii')
         self.validate(v)
         return v
 

commit ba52f341709f25b64c4edd05d5db1895835397f2

Fix fallback when zope.i18nmessageid is not installed
I introduced this regression in
https://github.com/zopefoundation/zope.schema/pull/118 by misreading the
try/except/else logic in `zope.schema._messageid`.  The new tests should
guard against somebody else making a similar mistake in future.

src/zope/schema/_messageid.py
@@ -14,7 +14,7 @@
 
 try:
     from zope.i18nmessageid import MessageFactory
-except ImportError:  # pragma: no cover
-    MessageFactory = str
-else:  # pragma: no cover
+except ImportError:
+    _ = str
+else:
     _ = MessageFactory("zope")

src/zope/schema/tests/test__messageid.py
@@ -1,0 +1,46 @@
+##############################################################################
+#
+# Copyright (c) 2023 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import importlib
+import types
+import unittest
+from unittest.mock import patch
+
+from zope.i18nmessageid import Message
+
+from zope.schema import _messageid
+
+
+class MessageIDTests(unittest.TestCase):
+
+    def test_with_zope_i18nmessageid(self):
+        # zope.i18nmessageid is installed while running tests, so `_`
+        # returns instances of `Message` by default.
+        message = _messageid._("test string")
+        self.assertIsInstance(message, Message)
+        self.assertEqual("zope", message.domain)
+        self.assertEqual("test string", message)
+
+    def test_without_zope_i18nmessageid(self):
+        # If we pretend that zope.i18nmessageid is not installed, then `_`
+        # falls back to returning instances of `str`.
+        module = types.ModuleType("zope.i18nmessageid")
+        try:
+            with patch.dict("sys.modules", {"zope.i18nmessageid": module}):
+                importlib.reload(_messageid)
+                message = _messageid._("test string")
+                self.assertNotIsInstance(message, Message)
+                self.assertEqual("test string", message)
+        finally:
+            importlib.reload(_messageid)